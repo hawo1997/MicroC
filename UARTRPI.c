f021cc27f15736c2cc239ae9d945cbc9ccba5075
hawo1997 <hawo1997@t-online.de>
2016-01-22 16:57:11 +0100

    Create UARTRPI.c

---
+++ UARTRPI.c
@@ -0,0 +1,16 @@
+//Program for an Atmega8 by Atmel, who will transmit data to and recieve data from a Raspberry Pi via integrated UART
+
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+	while(1)
+	{
+		//Space for code
+	}
+
+}
